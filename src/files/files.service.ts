f9f8cbea5448a4893b66d5b060e6d23a2f6b58f6
alatus17 <128619206+alatus17@users.noreply.github.com>
2025-03-30 19:08:37 +0700

    Add files via upload

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,23 +1,27 @@
-import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+
+import * as fs from 'fs'
 import * as path from 'path'
-import * as fs from 'fs';
-import * as uuid from 'uuid';
+import * as uuid from 'uuid'
+
+console.log(fs, path)
 
 @Injectable()
 export class FilesService {
-
-    async createFile(file): Promise<string> {
-        try {
-            const fileName = uuid.v4() + '.jpg';
-            const filePath = path.resolve(__dirname, '..', 'static')
-            if (!fs.existsSync(filePath)) {
-                fs.mkdirSync(filePath, {recursive: true})
-            }
-            fs.writeFileSync(path.join(filePath, fileName), file.buffer)
-            return fileName;
-        } catch (e) {
-            throw new HttpException('Произошла ошибка при записи файла', HttpStatus.INTERNAL_SERVER_ERROR)
-        }
+  async createFile(file): Promise<string> {
+    try {
+      const fileName = uuid.v4() + '.jpg'
+      const filePath = path.resolve(__dirname, '..', 'static')
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true })
+      }
+      fs.writeFileSync(path.join(filePath, fileName), file.buffer)
+      return fileName
+    } catch (err) {
+      throw new HttpException(
+        'Произошла ошибка при записи файла',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      )
     }
-
+  }
 }
