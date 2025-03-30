5b3094c02613a0b820ce982cf6016319643233df
alatus17 <128619206+alatus17@users.noreply.github.com>
2025-03-30 19:11:48 +0700

    Add files via upload

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,24 +1,23 @@
-import {forwardRef, Module} from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-import {SequelizeModule} from "@nestjs/sequelize";
-import {User} from "./users.model";
-import {Role} from "../roles/roles.model";
-import {UserRoles} from "../roles/user-roles.model";
-import {RolesModule} from "../roles/roles.module";
-import {AuthModule} from "../auth/auth.module";
-import {Post} from "../posts/posts.model";
+import { forwardRef, Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
+
+import { AuthModule } from '../auth/auth.module'
+import { Post } from '../posts/posts.model'
+import { Role } from '../roles/roles.model'
+import { RolesModule } from '../roles/roles.module'
+import { UserRoles } from '../roles/user-roles.model'
+import { UsersController } from './users.controller'
+import { User } from './users.model'
+import { UsersService } from './users.service'
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
-      SequelizeModule.forFeature([User, Role, UserRoles, Post]),
-      RolesModule,
-      forwardRef(() => AuthModule),
+    forwardRef(() => AuthModule),
+    SequelizeModule.forFeature([User, Role, UserRoles, Post]),
+    RolesModule,
   ],
-    exports: [
-        UsersService,
-    ]
+  exports: [UsersService],
 })
 export class UsersModule {}
