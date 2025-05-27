cf03545f657a4289c8d3a630d3930f3fe34652f3
alatus17 <128619206+alatus17@users.noreply.github.com>
2025-05-27 10:08:07 +0700

    Add files via upload

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,22 +1,39 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common'
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common'
 
-import { plainToClass } from 'class-transformer'
+import { plainToClass, plainToInstance } from 'class-transformer'
 import { validate } from 'class-validator'
 import { ValidationException } from 'exceptions/validation.exception'
 
 @Injectable()
-export class ValidationPipe implements PipeTransform<any> {
-  async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
-    const obj = plainToClass(metadata.metatype, value)
-    const errors = await validate(obj)
-
-    if (errors.length) {
-      console.log(errors)
-      const messages = errors.map((err) => {
-        return `${err.property} - ${Object.values(err.constraints).join(', ')}`
-      })
-      throw new ValidationException(messages)
+export class ValidationPipe implements PipeTransform {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.shouldValidate(metatype)) {
+      return value;
     }
-    return value
+
+    const obj = plainToInstance(metatype, value);
+
+    // Добавленная проверка
+    if (obj === null || typeof obj !== 'object') {
+      throw new BadRequestException('Invalid data format');
+    }
+
+    const errors = await validate(obj);
+
+    if (errors.length > 0) {
+      throw new BadRequestException(
+        errors.map(e => ({
+          field: e.property,
+          errors: e.constraints
+        }))
+      );
+    }
+
+    return obj;
+  }
+
+  private shouldValidate(metatype: any): boolean {
+    const excluded = [String, Boolean, Number, Array, Object];
+    return !excluded.includes(metatype);
   }
 }
